e68fc8bdd83565429f7096c60d60e8b7e6204683
vagrant <vagrant@dockerbox.local>
2019-02-20 15:55:36 +0000

    demo adjusted for new cf target

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]
   then
-    fly -t lite set-pipeline -p iot_demo -c ci/pipeline.yml -l ci/credentials.yml
+    fly -t lite set-pipeline -p cf_demo -c ci/pipeline.yml -l ci/credentials.yml
   else
-    fly -t $1 set-pipeline -p iot_demo -c ci/pipeline.yml -l ci/credentials.yml
+    fly -t $1 set-pipeline -p cf_demo -c ci/pipeline.yml -l ci/credentials.yml
 fi
